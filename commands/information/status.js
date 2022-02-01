32de70557023eadf606f8272a7559247f33ff262
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-02-01 22:01:45 +0700

    Update status.js

--- commands/information/status.js
+++ commands/information/status.js
@@ -7,21 +7,26 @@ module.exports = {
     async execute(interaction, client, createdTime) {
         try {
             const getStatus = await client.getStatus(await createdTime);
-            console.log(getStatus);
+            if (!getStatus || getStatus == null || getStatus == undefined) {
+                let sendMessage = ``;
+                sendMessage += `Uptime: **${await getStatus.uptime.days} Days : ${await getStatus.uptime.hours} Hours : ${await getStatus.uptime.minutes} Minutes : ${await getStatus.uptime.seconds} Seconds**\n`;
+                sendMessage += `Status: **${await getStatus.status}**\n`;
+                sendMessage += `Ping: **${await getStatus.ping.average} ms**\n`;
+                sendMessage += `\n`;
+                sendMessage += `Invite Link: **https://discord.com/oauth2/authorize?client_id=930354659493822515&scope=bot&permissions=27648860222**\n`;
+                sendMessage += `Website: **https://ingkth.wordpress.com/**\n`;
+                sendMessage += `Discord: **https://discord.gg/pbyWbUYjyt**\n`;
 
-            let sendMessage = ``;
-            sendMessage += `Uptime: **${await getStatus.uptime.days} Days : ${await getStatus.uptime.hours} Hours : ${await getStatus.uptime.minutes} Minutes : ${await getStatus.uptime.seconds} Seconds**\n`;
-            sendMessage += `Status: **${await getStatus.status}**\n`;
-            sendMessage += `Ping: **${await getStatus.ping.average} ms**\n`;
-            sendMessage += `\n`;
-            sendMessage += `Invite Link: **https://discord.com/oauth2/authorize?client_id=930354659493822515&scope=bot&permissions=27648860222**\n`;
-            sendMessage += `Website: **https://ingkth.wordpress.com/**\n`;
-            sendMessage += `Discord: **https://discord.gg/pbyWbUYjyt**\n`;
-
-            await interaction.editReply({
-                content: await sendMessage,
-                ephemeral: true
-            });
+                await interaction.editReply({
+                    content: await sendMessage,
+                    ephemeral: true
+                });
+            }else {
+                await interaction.editReply({
+                    content: `Something Went Wrong, Please Try Again Later`,
+                    ephemeral: true
+                });
+            }
         } catch (err) {
             console.error(err);
             await interaction.editReply({
